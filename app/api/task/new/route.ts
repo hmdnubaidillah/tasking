a7c9fc39f62cd3c98239d3fc698ee257e8d3a400
Nerots <daniuyan71@gmail.com>
2024-01-26 20:58:02 +0700

    add more routes

---
+++ app/api/task/new/route.ts
@@ -0,0 +1,28 @@
+import http from "http-status-codes";
+import { TaskType } from "@/types";
+import { prisma } from "@/lib/lib.db";
+import { taskSchema } from "@/helpers/helper.validation";
+import { Prisma } from "@prisma/client";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  try {
+    const validatedTask = await taskSchema.validate(body);
+    const newTask = await prisma.task.create({
+      data: {
+        author: { connect: { id: body.userId } },
+        name: validatedTask.name,
+        desc: validatedTask.desc,
+        dateDl: validatedTask.dateDl,
+        category: validatedTask.category,
+        importance: validatedTask.importance,
+      },
+    });
+
+    return Response.json({ task: newTask }, { status: http.CREATED });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error }, { status: http.INTERNAL_SERVER_ERROR });
+  }
+}
---
+++ app/api/task/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/lib.db";
+import http from "http-status-codes";
+
+// get all tasks
+export async function GET() {
+  try {
+    const tasks = await prisma.task.findMany();
+
+    if (!tasks) {
+      return Response.json({ message: "users not found" }, { status: http.NOT_FOUND });
+    }
+
+    return Response.json({ tasks }, { status: http.OK });
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({ error: error.message || "INTERNAL SERVER ERROR" }, { status: http.INTERNAL_SERVER_ERROR });
+    }
+  }
+}
